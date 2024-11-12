035dd613e54fdf913b73da4dccad955e217b44f2
pavel7991 <pasha.selin97@gmail.com>
2024-11-12 23:00:47 +0200

    add branch prorotype

---
+++ carousel.js
@@ -0,0 +1,124 @@
+function Carousel(containerId = 'carousel', slideClass = '.slide', interval = 2000) {
+  this.container = document.getElementById(containerId)
+  this.slides = this.container.querySelectorAll(slideClass)
+  this.INTERVAL = interval
+}
+
+
+Carousel.prototype = {
+  _initProps() {
+    this.currentSlide = 0
+    this.isPlaying = true
+
+    // carousel-controls
+    this.nextBtn = this.container.querySelector('#next-btn')
+    this.prevBtn = this.container.querySelector('#prev-btn')
+    this.pausePlayBtn = this.container.querySelector('#pause-play-btn')
+
+    //carousel-dots
+    this.carouselDots = this.container.querySelector('#carousel-dots')
+    this.dots = this.carouselDots.querySelectorAll('.dot')
+
+    // code keyboard
+    this.CODE_ARROW_LEFT = 'ArrowLeft'
+    this.CODE_ARROW_RIGHT = 'ArrowRight'
+    this.CODE_SPACE = 'Space'
+
+    // icons
+    this.ICON_PAUSE = '<i class="fa-solid fa-pause"></i>'
+    this.ICON_PLAY = '<i class="fa-solid fa-play"></i>'
+  },
+
+  _gotoSlide(n) {
+    this.slides[this.currentSlide].classList.toggle('active')
+    this.dots[this.currentSlide].classList.toggle('active')
+    this.currentSlide = (n + this.slides.length) % this.slides.length
+    this.slides[this.currentSlide].classList.toggle('active')
+    this.dots[this.currentSlide].classList.toggle('active')
+  },
+  _prevSlide() {
+    this._gotoSlide(this.currentSlide - 1)
+  },
+
+  _nextSlide() {
+    this._gotoSlide(this.currentSlide + 1)
+  },
+
+  pause() {
+    if (!this.isPlaying) return
+    clearInterval(this.timerId)
+    this.pausePlayBtn.innerHTML = this.ICON_PLAY
+    this.isPlaying = false
+  },
+
+  play() {
+    clearInterval(this.timerId);
+    this.pausePlayBtn.innerHTML = this.ICON_PAUSE
+    this.isPlaying = true
+    this._sliderAutoPlay()
+  },
+
+  pausePlay() {
+    this.isPlaying ? this.pause() : this.play()
+  },
+
+  next() {
+    this.pause()
+    this._nextSlide()
+  },
+
+  prev() {
+    this.pause()
+    this._prevSlide()
+  },
+
+  _dots(e) {
+    const { target } = e //  const target = e.target
+    if (target && target.classList.contains('dot')) {
+      this.pause()
+      this._gotoSlide(+target.dataset.slideTo)
+    }
+  },
+
+  _pressKey(e) {
+    const { code } = e
+    if (code === this.CODE_ARROW_RIGHT) this.next()
+    if (code === this.CODE_ARROW_LEFT) this.prev()
+    if (code === this.CODE_SPACE) {
+      e.preventDefault()
+      this.pausePlay()
+    }
+  },
+
+  _initListenners() {
+    this.nextBtn.addEventListener('click', this.next.bind(this))
+    this.prevBtn.addEventListener('click', this.prev.bind(this))
+    this.pausePlayBtn.addEventListener('click', this.pausePlay.bind(this))
+    this.carouselDots.addEventListener('click', this._dots.bind(this))
+    document.addEventListener('keydown', this._pressKey.bind(this))
+  },
+
+  _sliderAutoPlay() {
+    this.timerId = setInterval(() => this._nextSlide(), this.INTERVAL)
+  },
+
+  init() {
+    this._initProps()
+    this._initListenners()
+    this._sliderAutoPlay()
+  },
+}
+Carousel.prototype.constructor = Carousel
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ swipe-carousel.js
@@ -0,0 +1,34 @@
+function SwipeCarousel() {
+  Carousel.apply(this, arguments)
+  //this.slidesContainer = this.container && this.container.querySelector('#carousel-container')
+}
+
+SwipeCarousel.prototype = Object.create(Carousel.prototype)
+SwipeCarousel.prototype.constructor = SwipeCarousel
+
+SwipeCarousel.prototype._initListenners = function () {
+  Carousel.prototype._initListenners.apply(this);
+  this.container.addEventListener('touchstart', this.swipeStartHandler.bind(this))
+  this.container.addEventListener('mousedown', this.swipeStartHandler.bind(this))
+  this.container.addEventListener('touchend', this.swipeEndHandler.bind(this))
+  this.container.addEventListener('mouseup', this.swipeEndHandler.bind(this))
+  this.container.addEventListener('dragstart', this.swipeStartHandler.bind(this))
+  this.container.addEventListener('dragend', this.swipeEndHandler.bind(this))
+}
+
+SwipeCarousel.prototype.swipeStartHandler = function (e) {
+  this.startPosX = e instanceof MouseEvent
+    ? e.pageX // MouseEvent
+    : e.changedTouches[0].pageX //TouchEvent
+}
+
+SwipeCarousel.prototype.swipeEndHandler = function (e) {
+  this.endPosX = e instanceof MouseEvent
+    ? this.endPosX = e.pageX // MouseEvent
+    : e.changedTouches[0].pageX //TouchEvent
+  if (this.endPosX - this.startPosX > 100) this.prev()
+  if (this.endPosX - this.startPosX < -100) this.next()
+}
+
+
+
